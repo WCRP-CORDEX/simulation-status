050ecdc17d76de4353fff06123df31c99949e3dc
Jesus Fernandez <Jesus.Fernandez@unican.es>
2025-01-15 12:17:02 +0100

    Add both mip_era and fix domain order
    and other aesthetic changes

--- global_progress.py
+++ global_progress.py
@@ -4,9 +4,20 @@ import sys
 from icecream import ic
 
 experiment = sys.argv[1]
-status = ['published']#, 'completed', 'running', 'planned']
-colors = ['#3399FF']#, '#000000', '#009900', '#FF9999']
-url = "https://raw.githubusercontent.com/WCRP-CORDEX/simulation-status/main/docs/CORDEX_CMIP5_status.csv"
+mip_era = sys.argv[2]
+if mip_era == 'CMIP5':
+  url = "https://raw.githubusercontent.com/WCRP-CORDEX/simulation-status/main/docs/CORDEX_CMIP5_status.csv"
+else:
+  url = 'https://raw.githubusercontent.com/WCRP-CORDEX/simulation-status/main/CMIP6_downscaling_plans.csv'
+
+cordex_domains = ['SAM', 'CAM', 'NAM', 'EUR', 'MED', 'MENA', 'AFR', 'WAS', 'CAS', 'EAS', 'SEA', 'AUS', 'ANT', 'ARC']
+colors = {
+    'published': '#3399FF',
+    'completed': '#000000',
+    'running': '#009900',
+    'planned': '#FF9999'
+}
+status = colors.keys()
 
 def get_ymax(exp):
   if exp.startswith('ssp'):
@@ -14,27 +25,36 @@ def get_ymax(exp):
   elif exp.startswith('hist'):
     return(60)
   elif exp.startswith('eval'):
-    return(18)
+    return(25)
   else:
     return(None)
 
 df = pd.read_csv(url)
-df[['domain', 'resolution']] = df['domain'].str.split('-', expand = True)
-df['comments'] = df['resolution'] # Just to make the comments column exist
-df = df.query('resolution.str.endswith("44") | resolution.str.endswith("22") | resolution.str.endswith("11")') # avoid double-counting DOM-XXi sims
-filtered_df = df[~df['experiment'].isna() & df['experiment'].str.startswith(experiment) & ~df['comments'].str.match('#ESD', na = False)].query('status in @status')
+df[['domain', 'resolution']] = df['domain'].str.split('-', expand=True)
+if mip_era == 'CMIP5':
+  df['comments'] = ''  # Just to make the comments column exist
+  df = df[df['resolution'].notna()].query('~resolution.str.endswith("i")')  # avoid double-counting DOM-XXi sims
+filtered_df = df[
+    ~df['experiment'].isna()
+   & df['experiment'].str.startswith(experiment)
+  & ~df['comments'].str.match('#ESD', na=False)
+].query('status in @status')
+
 status_counts = filtered_df.groupby(['domain', 'status']).size().unstack(fill_value=0)
-status_counts = status_counts[status]
-status_counts = status_counts.loc[status_counts.sum(axis=1).sort_values(ascending=False).index]
+status_counts = status_counts.reindex(index=cordex_domains, columns=status, fill_value=0)
+status_counts = status_counts.loc[cordex_domains]
 ic(status_counts)
 ic(status_counts.sum(axis=1))
-status_counts.plot(kind='bar', stacked=True, figsize=(6, 4), color=colors)
+
+status_colors = [colors[s] for s in status]  # Match colors to the order of statuses
+status_counts.plot(kind='bar', stacked=True, figsize=(7, 2.5), color=status_colors, zorder=2)
 ax = plt.gca()
 ax.set_ylim([0, get_ymax(experiment)])
+ax.grid(axis='y', linestyle='--', linewidth=0.5, zorder=1)
 plt.xlabel('')
+plt.xticks(rotation=0)
 plt.ylabel('Total number of simulations')
 plt.title(f'Simulations by CORDEX domain ({experiment.upper()})')
-plt.legend(title='Status', loc='upper right')
+plt.legend(title='Status', loc='upper center')
 plt.tight_layout()
-plt.savefig(f'docs/CORDEX_CMIP5_global_progress__{experiment.upper()}.png', bbox_inches='tight')
-
+plt.savefig(f'docs/CORDEX_{mip_era}_global_progress__{experiment.upper()}.png', bbox_inches='tight')
